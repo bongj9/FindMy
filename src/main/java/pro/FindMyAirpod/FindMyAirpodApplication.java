50d368cd680b3a42daf8bdbd3c0e73dc1b975f23
bongj9 <lollypark501@naver.com>
2023-02-17 17:04:05 +0900

    초기 설정

---
+++ src/main/java/pro/FindMyAirpod/FindMyAirpodApplication.java
@@ -0,0 +1,13 @@
+package pro.FindMyAirpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyAirpodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyAirpodApplication.class, args);
+	}
+
+}
